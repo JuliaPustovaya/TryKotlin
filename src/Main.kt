c3afa17a6705645b47048bdb931fec8d08a7a3d6
JuliaPustovaya <pustovayajulia@gmail.com>
2018-08-15 18:46:34 +0300

    Функции

--- src/Main.kt
+++ src/Main.kt
@@ -16,19 +16,39 @@ fun main(args: Array<String>) {
 //    val st:String="immutable value "
 //    var st1:String="mutable"
 
-    var a1: String = "123"
+//    var a1: String = "123"
+//
+//    var a: Byte = 5        //8
+//    var a0: Short = 11 //16 bit
+//    var a2: Int = 123   //32 bit
+//    var a3: Long = 123  //64 bit
+//    var a4: Double = .189 //64 bit
+//    var a5: Float = 1.23F //32 bit
+//
+//
+//    //приведение типов
+//    var b1: Long = a2.toLong()
+//    var c1: Char = '4'
+//    var bool: Boolean =true
+    myFisrtFunction()
+    myFun2(5)
+    myFun3()
+    val result = myFun4(5, 2.7)
+    println(result)
 
-    var a: Byte = 5        //8
-    var a0: Short = 11 //16 bit
-    var a2: Int = 123   //32 bit
-    var a3: Long = 123  //64 bit
-    var a4: Double = .189 //64 bit
-    var a5: Float = 1.23F //32 bit
-
-
-    //приведение типов
-    var b1: Long = a2.toLong()
-    var c1: Char = '4'
-    var bool: Boolean =true
 
 }
+
+fun myFisrtFunction(): Int {
+    return 1;
+}
+
+fun myFun2(a: Long): Int {
+    return a.toInt()
+}
+
+fun myFun3() {
+}
+
+fun myFun4(a: Long, b: Double) = a + b
+
