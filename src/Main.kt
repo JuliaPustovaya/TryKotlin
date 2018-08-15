a983dba497722beddbbd9dd5c525c41ca3fe8082
JuliaPustovaya <pustovayajulia@gmail.com>
2018-08-15 18:22:22 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+    var name: String = "Alex"
+    var s: Int = 10
+    var b: Int = 4
+
+    println("Hello $name  !")
+    println("${s + b}")
+
+    val user = UserJAva("akak@shfj.com")
+    println("Java example: ${user.email}")
+   // userJAva.email="jskd"
+
+    val userKorlin=UserKotlin("testhdjh@.jgif")
+    println("Kotlin ${userKorlin.email}")
+
+    val st:String="immutable value "
+    var st1:String="mutable"
+
+
+
+
+}
---
+++ src/UserJAva.java
@@ -0,0 +1,11 @@
+public class UserJAva {
+	private final String email;
+
+	public UserJAva(String email) {
+		this.email = email;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+}
