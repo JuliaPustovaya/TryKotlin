7e768472e3959b9c75c452a9b722f85744920124
JuliaPustovaya <pustovayajulia@gmail.com>
2018-08-15 18:32:56 +0300

    типы данных

--- src/Main.kt
+++ src/Main.kt
@@ -6,17 +6,29 @@ fun main(args: Array<String>) {
     println("Hello $name  !")
     println("${s + b}")
 
-    val user = UserJAva("akak@shfj.com")
-    println("Java example: ${user.email}")
-   // userJAva.email="jskd"
+//    val user = UserJAva("akak@shfj.com")
+//    println("Java example: ${user.email}")
+//   // userJAva.email="jskd"
+//
+//    val userKorlin=UserKotlin("testhdjh@.jgif")
+//    println("Kotlin ${userKorlin.email}")
+//
+//    val st:String="immutable value "
+//    var st1:String="mutable"
 
-    val userKorlin=UserKotlin("testhdjh@.jgif")
-    println("Kotlin ${userKorlin.email}")
+    var a1: String = "123"
 
-    val st:String="immutable value "
-    var st1:String="mutable"
+    var a: Byte = 5        //8
+    var a0: Short = 11 //16 bit
+    var a2: Int = 123   //32 bit
+    var a3: Long = 123  //64 bit
+    var a4: Double = .189 //64 bit
+    var a5: Float = 1.23F //32 bit
 
 
-
+    //приведение типов
+    var b1: Long = a2.toLong()
+    var c1: Char = '4'
+    var bool: Boolean =true
 
 }
